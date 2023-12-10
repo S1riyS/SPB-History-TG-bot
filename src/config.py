9e1a9a7035b2022eaaef8d33a08f3315bb2f0c8b
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-10 17:51:28 +0300

    feat(config): added `STATIC_DIR` variable

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 # Directories of project
 SRC_DIR = Path(__file__).resolve().parent
 ROOT_DIR = SRC_DIR.parent
+STATIC_DIR = ROOT_DIR / "static"
 
 # Environment variables
 load_dotenv(ROOT_DIR / ".env")
