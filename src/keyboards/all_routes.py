708ffced275f67f717a9c56c1bf606be33d3ff38
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-10 17:59:31 +0300

    feat(keyboards): added keyboard with all routes

--- src/keyboards/all_routes.py
+++ src/keyboards/all_routes.py
@@ -1,4 +1,5 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
 from data import ROUTES
 from src.callbacks.factories.start_route import RouteCallbackFactory
 
@@ -7,7 +8,8 @@ def get_all_routes():
     builder = InlineKeyboardBuilder()
     for route_id, route_instance in ROUTES.items():
         builder.button(
-            text=route_instance.name, callback_data=RouteCallbackFactory(route_id=route_id)
+            text=route_instance.details.name,
+            callback_data=RouteCallbackFactory(route_id=route_id),
         )
     builder.adjust(1)
     return builder.as_markup()
