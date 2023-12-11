2c513d26a99494aabf01927f007dae3f3057bbb5
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-11 13:57:16 +0300

    chore(checkpoint): loading of photo is temporarily disabled

--- src/libs/checkpoint.py
+++ src/libs/checkpoint.py
@@ -5,7 +5,6 @@ from src.keyboards.checkpoint_next import get_next_keyboard
 from src.libs._renderable import Renderable
 from src.libs._typing import CheckpointDetails
 from src.libs.checkpoints_connector import CheckpointsConnector
-from src.utils.load_from_static import load_photo
 
 
 class Checkpoint(Renderable):
@@ -13,12 +12,13 @@ class Checkpoint(Renderable):
         self.details = details
         self.connector = connector
 
-    async def render(self, callback: CallbackQuery, data: CheckpointCallbackFactory = None) -> None:
-        photo = load_photo(self.details.photo_path)
-        await callback.message.answer_photo(photo)
+    async def render(
+            self, callback: CallbackQuery, data: CheckpointCallbackFactory = None
+    ) -> None:
+        # photo = load_photo(self.details.photo_path)
+        # await callback.message.answer_photo(photo)
 
         next_keyboard = get_next_keyboard(data.route_id, data.checkpoint_index + 1)
         await callback.message.answer(
-            self.details.description,
-            reply_markup=next_keyboard
+            self.details.description, reply_markup=next_keyboard
         )
