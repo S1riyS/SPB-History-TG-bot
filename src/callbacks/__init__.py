4e8985ca256a05d9775adb36c02737d35847d78c
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-11 13:52:37 +0300

    refactor(callbacks): `start_route` renamed to `route`

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -1,3 +1,3 @@
 from .checkpoint import send_checkpoint_info
 from .connector import send_connector_info
-from .start_route import send_starting_route_info
+from .route import send_starting_route_info
--- src/callbacks/route.py
+++ src/callbacks/route.py
@@ -6,7 +6,9 @@ from src.loader import dp
 
 
 @dp.callback_query(RouteCallbackFactory.filter())
-async def send_starting_route_info(callback: CallbackQuery, callback_data: RouteCallbackFactory):
+async def send_starting_route_info(
+        callback: CallbackQuery, callback_data: RouteCallbackFactory
+):
     current_route = ROUTES[callback_data.route_id]
     await current_route.render(callback, callback_data)
     await callback.answer()
