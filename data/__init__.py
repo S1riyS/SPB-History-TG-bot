095139c9f08b0a4e66e946fd04afeff411630ee0
Dingovina <kortkostya222@gmail.com>
2023-12-14 15:41:43 +0300

    Removed repeating routes.

--- data/__init__.py
+++ data/__init__.py
@@ -2,7 +2,6 @@ import typing as t
 
 from data.routes.route1 import route as first_route
 from data.routes.route2 import route as second_route
-from data.routes.route1 import route as third_route
 
 if t.TYPE_CHECKING:
     from src.libs.route import Route
@@ -10,5 +9,4 @@ if t.TYPE_CHECKING:
 ROUTES: t.Dict[int, "Route"] = {
     0: first_route,
     1: second_route,
-    2: third_route,
 }
