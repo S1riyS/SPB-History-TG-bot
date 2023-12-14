485485258ce1506b491c655f6983114ff071622a
jungsdine <125709080+jungsdine@users.noreply.github.com>
2023-12-14 15:36:00 +0300

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -2,6 +2,7 @@ import typing as t
 
 from data.routes.test_route import route as first_route
 from data.routes.route2 import route as second_route
+from data.routes.route1 import route as third_route
 
 if t.TYPE_CHECKING:
     from src.libs.route import Route
@@ -9,4 +10,5 @@ if t.TYPE_CHECKING:
 ROUTES: t.Dict[int, "Route"] = {
     0: first_route,
     1: second_route,
+    2: third_route,
 }
