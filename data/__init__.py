08dd7be6daf22dd22fed230d88018dab9e876f66
Dingovina <kortkostya222@gmail.com>
2023-12-14 15:25:18 +0300

    Fixed a lot of shit. Temporarily removes itinerary_comment.

--- data/__init__.py
+++ data/__init__.py
@@ -1,10 +1,12 @@
 import typing as t
 
 from data.routes.test_route import route as first_route
+from data.routes.route2 import route as second_route
 
 if t.TYPE_CHECKING:
     from src.libs.route import Route
 
 ROUTES: t.Dict[int, "Route"] = {
     0: first_route,
+    1: second_route,
 }
